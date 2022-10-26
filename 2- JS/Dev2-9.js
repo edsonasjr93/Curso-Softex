4b51c659b25d6d743d0f881bb408a95f59a15743
Edson Albuquerque Jr <101358910+edsonasjr93@users.noreply.github.com>
2022-10-26 16:59:21 -0300

    Update and rename Dev2-9 to Dev2-9.js

--- 2- JS/Dev2-9.js
+++ 2- JS/Dev2-9.js
@@ -1,10 +1,10 @@
-Crie um código com um objeto chamado “Banco”. Ele deverá ter propriedades que incluem conta, saldo, tipo de conta e agência e os seus métodos devem ser: buscar saldo, depósito, saque e número da conta.
+//Crie um código com um objeto chamado “Banco”. Ele deverá ter propriedades que incluem conta, saldo, tipo de conta e agência e os seus métodos devem ser: buscar saldo, depósito, saque e número da conta.
 
-Observações:
-- buscar saldo deve retornar o valor atual do saldo;
-- para o depósito, você deverá passar um valor como parâmetro e adicioná-lo no saldo final do objeto;
-- para o saque, você deverá passar um valor como parâmetro e subtraí-lo no saldo final do objeto;
-- o número da conta deve retornar o número da conta.
+//Observações:
+//- buscar saldo deve retornar o valor atual do saldo;
+//- para o depósito, você deverá passar um valor como parâmetro e adicioná-lo no saldo final do objeto;
+//- para o saque, você deverá passar um valor como parâmetro e subtraí-lo no saldo final do objeto;
+//- o número da conta deve retornar o número da conta.
 
 var banco = {
     conta: 1050, 
